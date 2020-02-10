63ed8f8d52ef721ddce90d7f417684ce2a042ed8
Jonny7 <jonatho.cooper7@gmail.com>
2020-02-10 19:45:06 -0400

    converted PG to querybuilder

--- Sources/ViiLibrary/ViiMySQLConnection.swift
+++ Sources/ViiLibrary/ViiMySQLConnection.swift
@@ -49,55 +49,55 @@ class ViiMySQLConnection: ViiConnection {
 
     func getPrimaryKey(table: Table) -> EventLoopFuture<Column?> {
         return self.connection.withConnection { db in
-        return db.sql()
-                 .raw("""
-                 SELECT
-                     kcu.column_name AS columnName,
-                     c.DATA_TYPE AS dataType,
-                     CASE WHEN c.IS_NULLABLE = 'NO' THEN
-                         FALSE
-                     ELSE
-                         TRUE
-                     END AS isNullable
-                 FROM
-                     information_schema.KEY_COLUMN_USAGE kcu
-                     INNER JOIN information_schema.columns c ON c.table_name = kcu.table_name
-                 WHERE
-                     kcu.table_schema = schema()
-                     AND constraint_name = 'PRIMARY'
-                    AND kcu.table_name = '\(bind: table.tableName)'
-                     AND kcu.column_name = c.column_name;
-                 """)
-                .first(decoding: Column.self)
+            return db.sql()
+                     .raw("""
+                     SELECT
+                         kcu.column_name AS columnName,
+                         c.DATA_TYPE AS dataType,
+                         CASE WHEN c.IS_NULLABLE = 'NO' THEN
+                             FALSE
+                         ELSE
+                             TRUE
+                         END AS isNullable,
+                         NULL AS constrainedTable
+                     FROM
+                         information_schema.KEY_COLUMN_USAGE kcu
+                         INNER JOIN information_schema.columns c ON c.table_name = kcu.table_name
+                     WHERE
+                         kcu.table_schema = schema()
+                         AND constraint_name = 'PRIMARY'
+                         AND kcu.table_name = 'demo'
+                         AND kcu.column_name = c.column_name;
+                     """)
+                    .first(decoding: Column.self)
         }
     }
     
     func getForeignKeys(table: Table) -> EventLoopFuture<[Column]> {
         return self.connection.withConnection { db in
-        return db.sql()
-                 .raw("""
-                 SELECT
-                     c.COLUMN_NAME AS columnName,
-                     c.DATA_TYPE AS dataType,
-                     CASE WHEN IS_NULLABLE = 'NO' THEN
-                         FALSE
-                     ELSE
-                         TRUE
-                     END AS isNullable
-                 FROM
-                     information_schema.columns c
-                 WHERE
-                     EXISTS (
-                         SELECT
-                             1
-                         FROM
-                             information_schema.TABLE_CONSTRAINTS tcc
-                         WHERE
-                             tcc. `TABLE_NAME` = c. `TABLE_NAME`
-                             AND tcc.CONSTRAINT_TYPE = 'FOREIGN KEY')
-                    AND c.table_name = '\(bind: table.tableName)'
-                 """)
-                .all(decoding: Column.self)
+            return db.sql()
+                     .raw("""
+                     SELECT
+                         c.COLUMN_NAME AS columnName,
+                         c.DATA_TYPE AS dataType,
+                         CASE WHEN IS_NULLABLE = 'NO' THEN
+                             FALSE
+                         ELSE
+                             TRUE
+                         END AS isNullable,
+                         kcu.REFERENCED_TABLE_NAME as constrainedTable
+                     FROM
+                         information_schema.TABLE_CONSTRAINTS tc
+                         INNER JOIN information_schema.KEY_COLUMN_USAGE kcu ON kcu.TABLE_NAME = tc.TABLE_NAME
+                         INNER JOIN information_schema.COLUMNS c ON tc.TABLE_NAME = c.TABLE_NAME
+                     WHERE
+                         tc.CONSTRAINT_TYPE = 'FOREIGN KEY'
+                         AND c.EXTRA != 'auto_increment'
+                         AND tc.TABLE_SCHEMA = 'vapor'
+                         AND c.TABLE_NAME = '\(table.tableName)'
+                         AND kcu.REFERENCED_TABLE_NAME IS NOT NULL;
+                     """)
+                    .all(decoding: Column.self)
         }
     }
 }
